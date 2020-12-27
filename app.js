b4039f98ab5a4d024158671ffcee3344cbd9df82
Gerald Ingraham <gingraham@Geralds-MacBook-Pro.local>
2020-12-26 18:47:09 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentloaded', () =>{
+
+function blocks() {
+    let el = document.createElement(div);
+    for (let i = 0; i <= 200; i++) {
+        const element = array[i];
+        
+    }
+    
+}
+
+
+})
